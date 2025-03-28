36fd987bbaefc6f37f4f996e3f083f23e81582cc
abdellatif guezadi <abdellatifguezadi@gmail.com>
2025-03-28 12:09:21 +0100

    creation de controller category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,12 +2,60 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
     public function index()
     {
-        return view('categories.index');
+        $categories = Category::all();
+        return view('admin.categories', compact('categories'));
     }
+
+    public function show(Category $category)
+    {
+        return view('categories.show', compact('category'));
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'type' => 'required|in:service,material'
+        ]);
+
+        Category::create($validated);
+
+        return redirect()->route('categories.index')->with('success', 'Catégorie ajoutée avec succès');
+    }
+
+    public function update(Request $request , Category $category)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'type' => 'required|in:service,material'
+        ]);
+
+        $category->update($validated);
+
+        return redirect()->route('categories.index')->with('success', 'Catégorie modifiée avec succès');
+    }
+
+    // public function destroy(Category $category)
+    // {
+    //     $category->delete();
+
+    //     return redirect()->route('categories.index')->with('success', 'Catégorie supprimée avec succès');
+    // }
+
+
+    // test test test test test test 
+    public function destroy (Category $category)
+    {
+        $category->delete();
+
+        return redirect()->route('categories.index')->with('success', 'Catégorie supprimée avec succès');
+    }
+   
 } 
